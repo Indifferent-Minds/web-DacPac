a3a2aac25a691e666b14478449560331c8265573
Pablo QB <qb.pablo.2000@gmail.com>
2025-02-09 13:07:46 +0100

    Mejoras header responsive

--- web-dacpac/src/componets/header/Header.tsx
+++ web-dacpac/src/componets/header/Header.tsx
@@ -1,33 +1,36 @@
 import './Header.css';
-const base = import.meta.env.VITE_BASE;     
+const base = import.meta.env.VITE_BASE; 
+import ReactIcon from '../../assets/react.svg'
 
 const NavBar = () => {
   return (
-    <div className="barranavegacion">
-      <div className="barranavegacion-social-icons">
-        <a href="https://instagram.com">
-          <img src={base+'instagram.svg'} alt="Instagram" draggable="false" />
+    <div className="barranavegacion row">
+      <div className="barranavegacion-social-icons col-4 d-none d-lg-block">
+        <a href="https://www.instagram.com/dacpac.campamento/" target='_blank' rel='noreferrer'>
+          <img src={ReactIcon} alt="Instagram" draggable="false" />
         </a>
-        <a href="https://x.com">
+        {/* <a href="https://x.com">
         <img src={base+'x.svg'} alt="x" draggable="false" />
         </a>
         <a href="https://threads.net">
           <img src={base+'threads.svg'} alt="Threads" draggable="false" />
-        </a>
+        </a> */}
       </div>
 
-      <div className="barranavegacion-logo">
+      <div className="barranavegacion-logo col-12 col-lg-4">
         <div className="logo-container">
           <img src={base+'logo.svg'} />
           <p>De Alta Capacidad Para Alta Capacidad</p>
         </div>
       </div>
 
-      <div className="barranavegacion-links">
-        <a href="#" className='enlace'>Inicio</a>
-        <a href="#">Actividades</a> 
-        <a href="#">AACC</a>
-        <a href="#">Contacto</a>
+      <div className="barranavegacion-links col-4 d-none d-lg-block">
+        <div className="d-flex justify-content-end">
+          <a href="#" className='mx-2'>Inicio</a>
+          <a href="#" className='mx-2'>Actividades</a> 
+          <a href="#" className='mx-2'>AACC</a>
+          <a href="#" className='mx-2'>Contacto</a>
+        </div>
       </div>
     </div>
   );
