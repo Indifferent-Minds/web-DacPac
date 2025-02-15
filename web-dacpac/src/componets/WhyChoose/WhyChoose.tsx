910d60ae6d01bf473babed60b884a61457ec326c
Jaime <jaimezpe.android@gmail.com>
2025-02-15 18:17:32 +0100

    "WhyChoose" image

--- web-dacpac/src/componets/WhyChoose/WhyChoose.tsx
+++ web-dacpac/src/componets/WhyChoose/WhyChoose.tsx
@@ -1,5 +1,8 @@
 import './WhyChoose.css';
 
+const base = import.meta.env.VITE_BASE; 
+const img = "whychoose.png";
+
 export const WhyChoose = () => {
   return (
     <center className="slide-in-right">
@@ -12,7 +15,7 @@ export const WhyChoose = () => {
           <p className="texto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non dignissim risus. Mauris iaculis justo ac ex dignissim aliquam. Quisque leo nisl, semper a dictum ac, ultrices vitae mauris. Sed quis lacus neque.</p>
         </div>
         <div className="img">
-          <img src="" draggable="false" alt="Imagenes de campamento" />
+          <img src={base + img} draggable="false" alt="Imagenes de campamento" className="imgen-whychoose" />
         </div>
       </div>
     </center>
--- web-dacpac/src/componets/main/Main.tsx
+++ web-dacpac/src/componets/main/Main.tsx
@@ -56,4 +56,3 @@ export default function Main() {
       </div>
     </main>
 }
- 
