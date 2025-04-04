6d7a55eab40c8c423d7472a6bba814bd895ceb49
Jaime <85058301+Jaimezpe@users.noreply.github.com>
2025-04-04 15:03:14 +0200

    Fix banner date

--- web-dacpac/src/componets/banner/Banner.tsx
+++ web-dacpac/src/componets/banner/Banner.tsx
@@ -10,7 +10,7 @@ export default function Banner({title, subTitles, extraText}: BannerProp) {
     return <section className='banner'>
         <div className="bg"></div>
         <h1>{title ?? 'Campamento Dac-Pac'}</h1>
-        <h2>{subTitles ?? 'Del 12 al 24 de julio'}</h2>
+        <h2>{subTitles ?? 'Del 12 al 25 de julio'}</h2>
         <h3>{extraText ?? '¡Vente, te espamos!'}</h3>
     </section>
 }
